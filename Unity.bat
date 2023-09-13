fbdc8326d7771527fd7ddb4c7846920b1364f3c4
Jeswin Kannampuzha Francis <jeswin.kannampuzha.francis@tuhh.de>
2023-09-13 19:02:55 +0200

    added nodered folder

--- Unity.bat
+++
@@ -1,5 +0,0 @@
-set UNITY_EXECUTABLE="C:\Program Files\Unity\Hub\Editor\2022.3.8f1\Editor\Unity.exe"
-set PROJECT_PATH="C:\Users\XR-Lab\Documents\Jeswin Files\Project HoloCFD\openfoamdocker\TestCFD"
-set METHOD_NAME="AssetBundleBuilder.AssetBundleBuilderFunction"
-
-%UNITY_EXECUTABLE% -batchmode -projectPath %PROJECT_PATH% -executeMethod %METHOD_NAME% -quit
